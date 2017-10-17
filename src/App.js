b1f6f1f1ace7d29273e4fce7796579d2637b6ae2
jpancho <jpancho@sfu.ca>
2017-10-16 18:40:11 -0700

    Add profileicon and reformat tabs.

--- src/App.js
+++ src/App.js
@@ -1,81 +1,96 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Navbar} from 'react-bootstrap';
+import {Jumbotron} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import axios from 'axios';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <p className="App-intro">
-                    <Form/>
-                </p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          <Form />
+        </p>
+      </div>
+    );
+  }
 }
 
 class SummonerInfo extends Component {
-    render() {
-        return (
-            <div className="Summoner Info">
-                <h3>{'Summoner: ' + this.props.name}</h3>
-                <h3>{'Level: ' + this.props.summonerLevel}</h3>
-            </div>
-        );
+  render() {
+    const URL = "http://ddragon.leagueoflegends.com/cdn/7.20.3/img/profileicon/";
+    if (this.props.name) {
+      return (
+        <div className="Summoner Info">
+          <h4>{'Summoner: ' + this.props.name}</h4>
+          <h4>{'Level: ' + this.props.summonerLevel}</h4>
+          <h4><img className="image"
+                   src={URL + this.props.profileIconId + ".png"} />
+          </h4>
+        </div>
+      )
+    } else {
+      return (
+        <div />
+      )
     }
+  }
 }
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            summoner: {
-                name: '',
-                summonerLevel: '',
-            }
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-    handleSubmit(event) {
-        event.preventDefault();
-        axios.get(`http://localhost:3001/api/${this.state.value}`)
-            .then(response => {
-                console.log(response);
-                this.setState({
-                    summoner: response.data
-                })
-            })
-            .catch(error => {
-                console.log('Error fetching and parsing data', error);
-            });
-    }
-    render() {
-        return (
-            <div className="Form">
-                <Navbar>
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            <textarea value={this.state.value}
-                                      onChange={this.handleChange}/>
-                            <input type="submit"
-                                   value="Submit"
-                                   placeholder="Enter Summoner"/>
-                        </label>
-                    </form>
-                </Navbar>
-                <SummonerInfo name={this.state.summoner.name}
-                              summonerLevel={this.state.summoner.summonerLevel}/>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      summoner: {
+        name: '',
+        summonerLevel: '',
+        profileIconId: '',
+      }
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.get(`http://localhost:3001/api/${this.state.value}`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          summoner: res.data
+        })
+      })
+      .catch(err => {
+        console.log('Error fetching and parsing data', err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="Form col-md-6 col-md-offset-3">
+        <Jumbotron>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text"
+                   placeholder="Enter Summoner.."
+                   value={this.state.value}
+                   onChange={this.handleChange} />
+            <input type="submit"
+                   value="Submit" />
+          </form>
+          <SummonerInfo name={this.state.summoner.name}
+                        summonerLevel={this.state.summoner.summonerLevel}
+                        profileIconId={this.state.summoner.profileIconId} />
+        </Jumbotron>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Server.js
+++ src/Server.js
@@ -1,7 +1,7 @@
 const leaguejs = require('leaguejs');
 const cors = require('cors');
 const express = require('express');
-const API_KEY = 'RGAPI-283e6bbb-0ec5-41de-b015-ae8a73d264c7';
+const API_KEY = 'RGAPI-557bd1de-65c8-4c46-928a-a4851b0cdd45';
 const api = new leaguejs(API_KEY);
 const app = express();
 
@@ -17,4 +17,14 @@ api.Summoner
     });
 });
 
+api.StaticData
+  .gettingProfileIcons(req.params.summonerName)
+  .then(data => {
+    return res.json(data);
+  })
+  .catch(err => {
+    return res.json(err);
+  });
+
 app.listen(3001);
+console.log('Running on port 3001');
